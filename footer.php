3bb8485788c7c6935c381921434182c6aaee41f3
bia0513 <104789428+bia0513@users.noreply.github.com>
2022-05-09 16:20:05 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
+	<?php wp_head(); ?>
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-12 rodape">
+			<p>Site Desenvolvido por Beatriz Nascimento</p>
+		</div>
+	</div>
+
+	</div>
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php get_header(); ?>
+
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+				<h2>Meu primeiro tema</h2>
+			</div>
+		</div>
+	</div>
+
+
+
+<?php get_footer(); ?>
+
+</body>
+</html>
